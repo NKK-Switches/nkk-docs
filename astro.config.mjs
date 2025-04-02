3fac8cd41a8ca4712cf0375aee63dd29a8ec5a0e
KeOndrae-NKK <141352366+KeOndrae-NKK@users.noreply.github.com>
2025-04-02 13:45:21 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://docs.nkkswitches.com/',
-	base: '/nkk-docs',
+	base: '/',
 	integrations: [
 		starlight({
 			title: 'NKK Switches Docs',
